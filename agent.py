1342622ad9a3515adb7b0427c84fb2221b49aaf7
izayahhudnut <84212540+izayahhudnut@users.noreply.github.com>
2023-10-10 00:08:01 -0500

    Update agent.py
    More reliable function calling and formatting

--- agent.py
+++ agent.py
@@ -12,7 +12,57 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+##To get the chat manager to call the function
+@dataclass
+class ExecutorGroupchat(GroupChat):
+    dedicated_executor: UserProxyAgent = None
 
+    def select_speaker(
+        self, last_speaker: ConversableAgent, selector: ConversableAgent
+    ):
+        """Select the next speaker."""
+
+        try:
+            message = self.messages[-1]
+            if "function_call" in message:
+                return self.dedicated_executor
+        except Exception as e:
+            print(e)
+            pass
+
+        selector.update_system_message(self.select_speaker_msg())
+        final, name = selector.generate_oai_reply(
+            self.messages
+            + [
+                {
+                    "role": "system",
+                    "content": f"Read the above conversation. Then select the next role from {self.agent_names} to play. Only return the role.",
+                }
+            ]
+        )
+        if not final:
+            # i = self._random.randint(0, len(self._agent_names) - 1)  # randomly pick an id
+            return self.next_agent(last_speaker)
+        try:
+            return self.agent_by_name(name)
+        except ValueError:
+            return self.next_agent(last_speaker)
+
+
+# Function for reading the CSV Menu
+def run_agent_query(query):
+    agent = create_csv_agent(
+        OpenAI(temperature=0),
+        "products.csv",
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
+    
+    
+    result = agent.run(query)
+    
+    
+    return result
 
 llm_config_menu = {
     "functions": [
@@ -44,7 +94,7 @@ comms_assistant = AssistantAgent(
 
 menu_assistant = AssistantAgent(
     name="Menu_Expert",
-    system_message="You are tasked with answering questions about the menu. You should answer the question based on the function given to you",
+    system_message="You are tasked with answering questions about the menu. You should answer the question based on the function given to you. Always add TERMINATE to the end of the recommendation",
     llm_config=llm_config_menu,
     )
 
@@ -57,42 +107,25 @@ order_assistant = AssistantAgent(
 user_proxy = autogen.UserProxyAgent(
    name="user_proxy",
    code_execution_config={"work_dir": "coding"},
+   is_termination_msg=lambda x: x.get("content", "") and x.get(
+       "content", "").rstrip().endswith("TERMINATE"),
+   human_input_mode="TERMINATE",
    system_message="Execute suggested function calls",
-)
-
-
-groupchat = autogen.GroupChat(agents=[user_proxy, comms_assistant, menu_assistant, order_assistant], messages=[], max_round=20)
-manager = autogen.GroupChatManager(groupchat=groupchat)
-
-
-# Function for reading the CSV Menu
-def run_agent_query(query):
-    agent = create_csv_agent(
-        OpenAI(temperature=0),
-        "products.csv",
-        verbose=True,
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    )
-    
-    
-    result = agent.run(query)
-    
-    
-    return result
-
-
-# register the functions
-user_proxy.register_function(
     function_map={
         "Menu": run_agent_query,
 
     }
 )
 
+
+groupchat = ExecutorGroupchat(agents=[user_proxy, comms_assistant, menu_assistant, order_assistant], messages=[], max_round=20, dedicated_executor = user_proxy)
+manager = GroupChatManager(groupchat=groupchat)
+
+
 # start the conversation
 user_proxy.initiate_chat(
     manager,
-    message="""I am ready to place a order""",
+    message="""what can i get that will make me happy for $15""",
 
 )
 
